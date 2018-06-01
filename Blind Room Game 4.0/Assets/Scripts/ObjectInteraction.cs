bfb6469bffecc09e733977d9af4865a0b97de367
Martijn van Ginkel <>
2018-06-01 13:14:54 +0200

    Script makes player able to interact with interactable objects

---
+++ Blind Room Game 4.0/Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObjectInteraction : MonoBehaviour {
+
+	protected bool objectTriggered;
+	public bool inTrigger;
+	private bool allowedToTrigger;
+
+	[SerializeField]
+	protected float waitTime;
+
+	protected virtual void Start () {
+		allowedToTrigger = true;
+	}
+
+	protected virtual void Update() { 
+		
+		if (inTrigger) {
+			if (Input.GetKeyDown(KeyCode.F)) {
+				StartCoroutine("TriggerObjectCo");
+				allowedToTrigger = false;
+			}
+		}
+	}
+
+	IEnumerator TriggerObjectCo() {
+		objectTriggered = !objectTriggered;
+		yield return new WaitForSeconds(waitTime);
+		allowedToTrigger = true;
+		inTrigger = false;
+	}
+
+}
