777a62413bcd46645d1295d2ccbac3b7cbcbdc93
Martijn van Ginkel <>
2018-06-01 13:13:53 +0200

    Added animation and soundscript that inherit from objectinteraction

---
+++ Blind Room Game 4.0/Assets/Scripts/InteractSound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractSound : ObjectInteraction {
+
+	private AudioSource audioSource;
+
+	[SerializeField]
+	private AudioClip soundEffect;
+
+	// Use this for initialization
+	protected override void Start () {
+
+		audioSource = GetComponent<AudioSource>();
+
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+
+		if (base.objectTriggered) {
+			Debug.Log("PlaySound");
+            PlaySound();
+		}
+
+		base.Update();
+	}
+
+	void PlaySound () {
+		audioSource.clip = soundEffect;
+		audioSource.Play();
+		base.objectTriggered = false;
+	}
+}
