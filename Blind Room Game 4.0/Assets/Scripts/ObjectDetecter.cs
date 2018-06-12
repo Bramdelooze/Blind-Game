4489e9aaeda1e6b34ddeb22cfb90ff32413e6770
Martijn van Ginkel <>
2018-06-12 10:42:50 +0200

    Checkout

--- Blind Room Game 4.0/Assets/Scripts/ObjectDetecter.cs
+++ Blind Room Game 4.0/Assets/Scripts/ObjectDetecter.cs
@@ -11,41 +11,7 @@ public class ObjectDetecter : MonoBehaviour {
     private bool animationPlaying;
     private static bool gotKey;
     public GameObject drawerAnimCont;
-    /*
-        // Use this for initialization
-        void Start () {
-            fadeAnim = gameObject.GetComponent<Animator>();
-            meshRenderer = GetComponent<MeshRenderer>();
-            meshRenderer.enabled = false;
-        }
 
-        IEnumerator ShowMeCo () {
-
-            //fadeAnim.Play("Fade-Out");
-            //animationPlaying = true;
-            //Color originalCol = meshRenderer.material.GetColor("_Color");
-            //Color col = meshRenderer.material.GetColor("_Color");
-            //col.a = 0;
-            //meshRenderer.material.color = Color.Lerp(originalCol, col, Time.deltaTime);
-            yield return new WaitForSeconds(.5f);
-            meshRenderer.enabled = false;
-
-        }
-
-        void OnTriggerEnter(Collider other)
-        {
-               //StopCoroutine("ShowMeCo");
-               //if (animationPlaying)
-               //{
-               //    fadeAnim.Play("Idle");
-               //    animationPlaying = false;
-               //}
-                Color32 col = meshRenderer.material.GetColor("_Color");
-                col.a = 100;
-                meshRenderer.material.SetColor("_Color", col);
-                meshRenderer.enabled = true;
-        }
-        */
     private void OnTriggerStay(Collider other)
     {
         if(gameObject.name == "Key" && Input.GetKeyDown(KeyCode.Mouse0) && drawerAnimCont.GetComponent<DrawerAnimationController>().slidedOut)
@@ -64,10 +30,5 @@ public class ObjectDetecter : MonoBehaviour {
             Destroy(gameObject);
         }
     }
-    /*
-    private void OnTriggerExit(Collider other)
-    {
-        StartCoroutine("ShowMeCo");
-    }
-    */
+
 }
